93b37276f9fa7fe2ac244cae695ed6d538a817a0
Evan <daley.evan@gmail.com>
2025-10-04 20:19:12 -0700

    WIP

--- src/scenes/color_palette_3/ColorTheory3.js
+++ src/scenes/color_palette_3/ColorTheory3.js
@@ -1,42 +1,24 @@
 import React, { useState, useRef } from "react"
-import { useFrame } from "@react-three/fiber"
-import { Html, Sphere, Torus, Cone, Cylinder, Box } from "@react-three/drei"
-import { EffectComposer, Bloom, Vignette } from "@react-three/postprocessing"
 import OrthoV2 from "../../components/controls/OrthoV2"
 import SimpleLighting from "../../components/environment/SimpleLighting"
-import ColorPalettes from "./ColorPalettes"
-import FloatingEffect from "./FloatingEffect";
 import EffectsV1 from "../../components/effects/EffectsV1";
-import RandomFloatingObjects from "./RandomFloatingObjects";
-import PaletteInfoOverlay from "./PaletteInfoOverlay";
+import RandomFloatingObjects from "./objects/RandomFloatingObjects";
+import PaletteInfoOverlay from "./html/PaletteInfoOverlay";
 import DynamicPaletteDrone from "../../components/props/dynamic_palette/DynamicPaletteDrone";
+import usePaletteControls from "./hooks/usePaletteControls";
 
 export default function Scene() {
-    const palettes = ColorPalettes()
-    const [mode, setMode] = useState("monochrome")
-    const palette = palettes[mode].colors
-    const label = palettes[mode].label
-
-    const cycleMode = () => {
-        const keys = Object.keys(palettes)
-        const currentIndex = keys.indexOf(mode)
-        const nextMode = keys[(currentIndex + 1) % keys.length]
-        setMode(nextMode)
-    }
+    const { mode, palette, label, cycleMode } = usePaletteControls();
 
     return (
         <>
-
             <OrthoV2 />
             <SimpleLighting ambientIntensity={0.6} directionalIntensity={2.0} />
-            <EffectsV1/>
+            <EffectsV1 />
             <color attach="background" args={["#222"]} />
-
-            <RandomFloatingObjects palette={palette} />
+            {/*<RandomFloatingObjects palette={palette} />*/}
             <DynamicPaletteDrone palette={palette} />
-
-
             <PaletteInfoOverlay mode={mode} label={label} onSwitch={cycleMode} />
         </>
-    )
+    );
 }
